b4b343571bcb2cefac26744fd49ea5a65a0b4ae2
Valenti25 <161212070+Valenti25@users.noreply.github.com>
2024-11-16 08:24:06 -0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import { Elysia } from "elysia";
+import { gettransports } from "./model";
+import { cors } from '@elysiajs/cors';
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().use(cors());
+app
+  .get("/transports", () => gettransports())
+  .post("/transports", ({ body }) => {  // Corrected here
+    console.log("body", body);
+    return { message: "ok" };
+  })
+  .listen(5000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
--- src/model.ts
+++ src/model.ts
@@ -1,5 +1,85 @@
 import { Database } from "bun:sqlite";
 
 const db = new Database('mydb.sqlite');
-const query = db.query("select 'Hello world' as message;");
-console.log(query.get())
+
+const gettransports = () => {
+    try {
+        const query = db.query("SELECT * from transport;");
+        return query.all();
+    } catch (error) {
+        console.log('error', error);
+        return [];
+    }
+};
+
+const gettransport = (id: number) => {
+    try {
+        const query = db.query("SELECT * from transport where id=$id;");
+        return query.get({
+            $id: id
+        });
+    } catch (error) {
+        console.log('error', error);
+        return [];
+    }
+};
+
+const createtransport = (transport: any) => {
+    try {
+        const query = db.query(`
+            INSERT INTO "transport" 
+            ("shopname", "pro_name", "address", "pro_quantity") 
+            VALUES ($shopname, $pro_name, $address, $pro_quantity)
+        `);
+        
+        return query.run({
+            $shopname: transport.shopname,
+            $pro_name: transport.pro_name,
+            $address: transport.address,
+            $pro_quantity: transport.pro_quantity
+        });
+    } catch (error) {
+        console.log('error', error);
+    }
+};
+const updatetransport = (id: number, transport: any) => {
+    try {
+        const query = db.query(`
+            UPDATE "transport" 
+            SET "shopname"=$shopname, 
+                "pro_name"=$pro_name, 
+                "address"=$address, 
+                "pro_quantity"=$pro_quantity 
+            WHERE "id"=$id
+    `);
+        
+        return query.run({
+            $id: id,
+            $shopname: transport.shopname,
+            $pro_name: transport.pro_name,
+            $address: transport.address,
+            $pro_quantity: transport.pro_quantity
+        });
+    } catch (error) {
+        console.log('error', error);
+    }
+};
+const deletetransport = (id: number) => {
+    try {
+        const query = db.query(`
+           DELETE FROM transport WHERE id=$id; `);
+           query.run ({
+            $id: id
+           })
+    } catch (error) {
+        console.log('error', error);
+    }
+};
+
+export {
+    gettransports,
+    gettransport,
+    createtransport,
+    updatetransport,
+    deletetransport
+}
